438d68a0f53d31b24fb718896f3366bbecb0bc17
N4shM <nashwahmahomed@gmail.com>
2026-05-17 21:20:13 +0200

    TreatmentMedicine

---
+++ Dental Practice Management System/TreatmentMedicine.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Dental_Practice_Management_System
+{
+    partial class TreatmentMedicine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TreatmentMedicine));
+            this.btnAddTreatment = new System.Windows.Forms.Button();
+            this.btnPrescribeMedication = new System.Windows.Forms.Button();
+            this.btnTreatmentHistory = new System.Windows.Forms.Button();
+            this.btnAddDiagnosis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddTreatment
+            // 
+            this.btnAddTreatment.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddTreatment.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddTreatment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddTreatment.ForeColor = System.Drawing.Color.Transparent;
+            this.btnAddTreatment.Location = new System.Drawing.Point(177, 210);
+            this.btnAddTreatment.Name = "btnAddTreatment";
+            this.btnAddTreatment.Size = new System.Drawing.Size(206, 37);
+            this.btnAddTreatment.TabIndex = 0;
+            this.btnAddTreatment.UseVisualStyleBackColor = false;
+            // 
+            // btnPrescribeMedication
+            // 
+            this.btnPrescribeMedication.BackColor = System.Drawing.Color.Transparent;
+            this.btnPrescribeMedication.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPrescribeMedication.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrescribeMedication.ForeColor = System.Drawing.Color.Transparent;
+            this.btnPrescribeMedication.Location = new System.Drawing.Point(442, 210);
+            this.btnPrescribeMedication.Name = "btnPrescribeMedication";
+            this.btnPrescribeMedication.Size = new System.Drawing.Size(206, 37);
+            this.btnPrescribeMedication.TabIndex = 1;
+            this.btnPrescribeMedication.UseVisualStyleBackColor = false;
+            // 
+            // btnTreatmentHistory
+            // 
+            this.btnTreatmentHistory.BackColor = System.Drawing.Color.Transparent;
+            this.btnTreatmentHistory.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTreatmentHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTreatmentHistory.ForeColor = System.Drawing.Color.Transparent;
+            this.btnTreatmentHistory.Location = new System.Drawing.Point(709, 210);
+            this.btnTreatmentHistory.Name = "btnTreatmentHistory";
+            this.btnTreatmentHistory.Size = new System.Drawing.Size(206, 37);
+            this.btnTreatmentHistory.TabIndex = 2;
+            this.btnTreatmentHistory.UseVisualStyleBackColor = false;
+            // 
+            // btnAddDiagnosis
+            // 
+            this.btnAddDiagnosis.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddDiagnosis.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddDiagnosis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddDiagnosis.ForeColor = System.Drawing.Color.Transparent;
+            this.btnAddDiagnosis.Location = new System.Drawing.Point(994, 210);
+            this.btnAddDiagnosis.Name = "btnAddDiagnosis";
+            this.btnAddDiagnosis.Size = new System.Drawing.Size(206, 37);
+            this.btnAddDiagnosis.TabIndex = 3;
+            this.btnAddDiagnosis.UseVisualStyleBackColor = false;
+            // 
+            // TreatmentMedicine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1262, 753);
+            this.Controls.Add(this.btnAddDiagnosis);
+            this.Controls.Add(this.btnTreatmentHistory);
+            this.Controls.Add(this.btnPrescribeMedication);
+            this.Controls.Add(this.btnAddTreatment);
+            this.Name = "TreatmentMedicine";
+            this.Text = "TreatmentMedicine";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddTreatment;
+        private System.Windows.Forms.Button btnPrescribeMedication;
+        private System.Windows.Forms.Button btnTreatmentHistory;
+        private System.Windows.Forms.Button btnAddDiagnosis;
+    }
+}
