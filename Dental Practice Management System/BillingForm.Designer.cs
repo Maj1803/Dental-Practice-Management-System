6dedcdf77cdb4af9aa3d80f0f6edabe633b8a733
katelyngovender767 <224125223@stu.ukzn.ac.za>
2026-05-25 07:51:18 +0200

    billing update

--- Dental Practice Management System/BillingForm.Designer.cs
+++ Dental Practice Management System/BillingForm.Designer.cs
@@ -28,198 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtID = new System.Windows.Forms.TextBox();
             this.btnTreatment = new System.Windows.Forms.Button();
-            this.txtApptID = new System.Windows.Forms.TextBox();
+            this.dgvTreatment = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
             this.btnInvoice = new System.Windows.Forms.Button();
-            this.cmbMethod = new System.Windows.Forms.ComboBox();
-            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.btnSave = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnClear = new System.Windows.Forms.Button();
-            this.dsDentist = new Dental_Practice_Management_System.dsDentist();
-            this.treatmentBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.treatmentTableAdapter = new Dental_Practice_Management_System.dsDentistTableAdapters.TreatmentTableAdapter();
-            this.treatmentNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.treatmentCostDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lblTotal = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dsDentist)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.treatmentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTreatment)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // txtID
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.treatmentNameDataGridViewTextBoxColumn,
-            this.treatmentCostDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.treatmentBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(201, 198);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(304, 150);
-            this.dataGridView1.TabIndex = 1;
+            this.txtID.Location = new System.Drawing.Point(220, 98);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 0;
             // 
             // btnTreatment
             // 
-            this.btnTreatment.Location = new System.Drawing.Point(201, 160);
+            this.btnTreatment.Location = new System.Drawing.Point(220, 152);
             this.btnTreatment.Name = "btnTreatment";
-            this.btnTreatment.Size = new System.Drawing.Size(149, 23);
-            this.btnTreatment.TabIndex = 2;
-            this.btnTreatment.Text = "Load Treatments";
+            this.btnTreatment.Size = new System.Drawing.Size(137, 23);
+            this.btnTreatment.TabIndex = 1;
+            this.btnTreatment.Text = "Load Treatment";
             this.btnTreatment.UseVisualStyleBackColor = true;
             // 
-            // txtApptID
+            // dgvTreatment
             // 
-            this.txtApptID.Location = new System.Drawing.Point(201, 106);
-            this.txtApptID.Name = "txtApptID";
-            this.txtApptID.Size = new System.Drawing.Size(100, 22);
-            this.txtApptID.TabIndex = 3;
+            this.dgvTreatment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTreatment.Location = new System.Drawing.Point(211, 228);
+            this.dgvTreatment.Name = "dgvTreatment";
+            this.dgvTreatment.RowHeadersWidth = 51;
+            this.dgvTreatment.RowTemplate.Height = 24;
+            this.dgvTreatment.Size = new System.Drawing.Size(194, 137);
+            this.dgvTreatment.TabIndex = 2;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(372, 402);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(44, 16);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "label1";
             // 
             // btnInvoice
             // 
-            this.btnInvoice.Location = new System.Drawing.Point(201, 446);
+            this.btnInvoice.Location = new System.Drawing.Point(220, 471);
             this.btnInvoice.Name = "btnInvoice";
             this.btnInvoice.Size = new System.Drawing.Size(75, 23);
             this.btnInvoice.TabIndex = 4;
             this.btnInvoice.Text = "Invoice";
             this.btnInvoice.UseVisualStyleBackColor = true;
             // 
-            // cmbMethod
+            // textBox2
             // 
-            this.cmbMethod.FormattingEnabled = true;
-            this.cmbMethod.Location = new System.Drawing.Point(767, 198);
-            this.cmbMethod.Name = "cmbMethod";
-            this.cmbMethod.Size = new System.Drawing.Size(121, 24);
-            this.cmbMethod.TabIndex = 5;
+            this.textBox2.Location = new System.Drawing.Point(543, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
             // 
-            // txtAmount
+            // comboBox1
             // 
-            this.txtAmount.Location = new System.Drawing.Point(767, 267);
-            this.txtAmount.Name = "txtAmount";
-            this.txtAmount.Size = new System.Drawing.Size(100, 22);
-            this.txtAmount.TabIndex = 7;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(543, 199);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(543, 274);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(767, 348);
+            this.dateTimePicker1.Location = new System.Drawing.Point(543, 354);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
             this.dateTimePicker1.TabIndex = 8;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(773, 439);
+            this.btnSave.Location = new System.Drawing.Point(579, 441);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 9;
-            this.btnSave.Text = "button1";
+            this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(767, 160);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 10;
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(926, 512);
+            this.btnClear.Location = new System.Drawing.Point(826, 526);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 23);
-            this.btnClear.TabIndex = 11;
+            this.btnClear.TabIndex = 10;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
-            // 
-            // dsDentist
-            // 
-            this.dsDentist.DataSetName = "dsDentist";
-            this.dsDentist.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // treatmentBindingSource
-            // 
-            this.treatmentBindingSource.DataMember = "Treatment";
-            this.treatmentBindingSource.DataSource = this.dsDentist;
-            // 
-            // treatmentTableAdapter
-            // 
-            this.treatmentTableAdapter.ClearBeforeFill = true;
-            // 
-            // treatmentNameDataGridViewTextBoxColumn
-            // 
-            this.treatmentNameDataGridViewTextBoxColumn.DataPropertyName = "TreatmentName";
-            this.treatmentNameDataGridViewTextBoxColumn.HeaderText = "TreatmentName";
-            this.treatmentNameDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.treatmentNameDataGridViewTextBoxColumn.Name = "treatmentNameDataGridViewTextBoxColumn";
-            this.treatmentNameDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // treatmentCostDataGridViewTextBoxColumn
-            // 
-            this.treatmentCostDataGridViewTextBoxColumn.DataPropertyName = "TreatmentCost";
-            this.treatmentCostDataGridViewTextBoxColumn.HeaderText = "TreatmentCost";
-            this.treatmentCostDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.treatmentCostDataGridViewTextBoxColumn.Name = "treatmentCostDataGridViewTextBoxColumn";
-            this.treatmentCostDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // lblTotal
-            // 
-            this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(467, 378);
-            this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(38, 16);
-            this.lblTotal.TabIndex = 12;
-            this.lblTotal.Text = "Total";
             // 
             // BillingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Dental_Practice_Management_System.Properties.Resources.System_Background;
-            this.ClientSize = new System.Drawing.Size(1066, 797);
-            this.Controls.Add(this.lblTotal);
+            this.ClientSize = new System.Drawing.Size(920, 796);
             this.Controls.Add(this.btnClear);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.txtAmount);
-            this.Controls.Add(this.cmbMethod);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.btnInvoice);
-            this.Controls.Add(this.txtApptID);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.dgvTreatment);
             this.Controls.Add(this.btnTreatment);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtID);
             this.Name = "BillingForm";
             this.Text = "BillingForm";
-            this.Load += new System.EventHandler(this.BillingForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dsDentist)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.treatmentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTreatment)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.DataGridView dataGridView1;
+
+        private System.Windows.Forms.TextBox txtID;
         private System.Windows.Forms.Button btnTreatment;
-        private System.Windows.Forms.TextBox txtApptID;
+        private System.Windows.Forms.DataGridView dgvTreatment;
+        private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Button btnInvoice;
-        private System.Windows.Forms.ComboBox cmbMethod;
-        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnClear;
-        private dsDentist dsDentist;
-        private System.Windows.Forms.BindingSource treatmentBindingSource;
-        private dsDentistTableAdapters.TreatmentTableAdapter treatmentTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn treatmentNameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn treatmentCostDataGridViewTextBoxColumn;
-        private System.Windows.Forms.Label lblTotal;
     }
 }
--- Dental Practice Management System/Invoice.cs
+++ Dental Practice Management System/Invoice.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Dental_Practice_Management_System
 {
-    public partial class BillingForm : Form
+    public partial class Invoice : Form
     {
-        public BillingForm()
+        public Invoice()
         {
             InitializeComponent();
         }
